883008f6987264bcc7b1de906c1625cf3702e59c
Lillie Sauer <lillie.sauer@wwt.com>
2022-05-16 13:46:03 -0500

    Add more tests around recently published Questions and make tests pass

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -15,8 +15,9 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
+        now = timezone.now()
         # noinspection PyTypeChecker
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
