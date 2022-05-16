14ea532bea85b9635a7f7f5afb76b453f9912003
Lillie Sauer <lillie.sauer@wwt.com>
2022-05-16 14:44:14 -0500

    Add more tests to test index view and update templates to make tests pass

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
-
 
 from .models import Choice, Question
 
@@ -11,10 +11,14 @@ class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
 
-    @staticmethod
-    def get_queryset():
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
@@ -47,4 +51,4 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=question.id))
